f706da276b82a0d8125a352ce2ab7045fe57e89b
hadeelwadia <hadeelwadia@gmail.com>
2024-11-17 12:33:23 +0200

    create folder structure

---
+++ script.js
@@ -0,0 +1,14 @@
+const getTotalNumberWithAvarage=(numbers)=>{
+numbers.reduce((sum,t)=>{
+    let conut=sum
+console.log(`total:${t}avarge:${sum.lenght}`) 
+})
+}
+const removeDepleicateString=(string)=>{
+    const stringWithoutDep=Array.isArray(string)
+    stringWithoutDep.set()
+    return stringWithoutDep
+}
+
+
+
